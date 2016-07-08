763f7fba1257dbeff66270ae5533f9c461b6884a
Mervyn McCreight <mervyn.mccreight@gmail.com>
2016-07-08 11:37:08 +0200

    introduce test for game state saving.

--- src/main/java/de/merv/rock_paper_scissors/Game.java
+++ src/main/java/de/merv/rock_paper_scissors/Game.java
@@ -1,10 +1,23 @@
 package de.merv.rock_paper_scissors;
 
 public class Game {
+
+    public Game(Player playerOne, Player playerTwo) {
+
+    }
+
     public void run() {
     }
 
     public int playedGames() {
         return 100;
+    }
+
+    public int playerOneWins() {
+        return 0;
+    }
+
+    public int playerTwoWins() {
+        return 0;
     }
 }
--- src/test/groovy/de/merv/rock_paper_scissors/GameSpec.groovy
+++ src/test/groovy/de/merv/rock_paper_scissors/GameSpec.groovy
@@ -6,10 +6,21 @@ class GameSpec extends Specification {
 
     def "a game should consist of 100 runs"() {
         when:
-        Game game = new Game();
+        Game game = new Game(new Player({ Hand.ROCK }), new Player({ Hand.ROCK }));
         game.run();
 
         then:
         game.playedGames() == 100;
+    }
+
+    def "a rock player always wins against a scissors player"() {
+        when:
+        Game game = new Game(new Player({ Hand.ROCK }), new Player({ Hand.SCISSORS }));
+        game.run();
+
+        then:
+        game.playedGames() == 100;
+        game.playerOneWins() == 100;
+        game.playerTwoWins() == 0;
     }
 }
