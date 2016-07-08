b8266c20ac48f5f8a6bd5ac65a57f20cfbbd7c6a
Mervyn McCreight <mervyn.mccreight@gmail.com>
2016-07-08 11:51:05 +0200

    game now actually simulates a game and knows the # of games each player has won.

--- src/main/java/de/merv/rock_paper_scissors/Game.java
+++ src/main/java/de/merv/rock_paper_scissors/Game.java
@@ -1,23 +1,38 @@
 package de.merv.rock_paper_scissors;
 
+import java.util.LinkedList;
+import java.util.List;
+import java.util.stream.IntStream;
+
 public class Game {
 
-    public Game(Player playerOne, Player playerTwo) {
+    private static final int RUNS = 100;
 
+    private final Player playerOne;
+    private final Player playerTwo;
+
+    private List<GameResult> results = new LinkedList<>();
+
+    public Game(Player playerOne, Player playerTwo) {
+        this.playerOne = playerOne;
+        this.playerTwo = playerTwo;
     }
 
     public void run() {
+        IntStream.range(0, RUNS).forEach(
+                i -> results.add(playerOne.plays().plays(playerTwo.plays()))
+        );
     }
 
-    public int playedGames() {
-        return 100;
+    public long playedGames() {
+        return results.size();
     }
 
-    public int playerOneWins() {
-        return 0;
+    public long playerOneWins() {
+        return results.stream().filter(r -> r.equals(GameResult.WIN)).count();
     }
 
-    public int playerTwoWins() {
-        return 0;
+    public long playerTwoWins() {
+        return results.stream().filter(r -> r.equals(GameResult.LOSE)).count();
     }
 }
